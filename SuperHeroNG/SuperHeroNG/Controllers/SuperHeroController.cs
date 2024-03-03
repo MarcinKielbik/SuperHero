3dfcb7cec1c8a0d82a3c53220d3fe11c1027e5f7
Marcin Kielbik <marcinlukasz.kielbik@gmail.com>
2024-03-03 20:28:16 +0100

    Creating UI and API

---
+++ SuperHeroNG/SuperHeroNG/Controllers/SuperHeroController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SuperHeroNG.Data;
+using SuperHeroNG.Models;
+
+namespace SuperHeroNG.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public SuperHeroController(DataContext context) 
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> GetAll()
+        {
+            return Ok(await _context.SuperHeroes.ToListAsync());
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<SuperHero>>> AddSuperHeroes(SuperHero hero)
+        {
+            _context.SuperHeroes.Add(hero);
+            return Ok(await _context.SaveChangesAsync());
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<SuperHero>>> UpdateSuperHeroes(SuperHero hero)
+        {
+            var dbHero = await _context.SuperHeroes.FindAsync(hero.Id);
+            if (dbHero == null)
+            {
+               return BadRequest("Hero not found.");
+            }
+            dbHero.Name = hero.Name;
+            dbHero.FirstName = hero.FirstName;
+            dbHero.LastName = hero.LastName;
+            dbHero.Place = hero.Place;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.SuperHeroes.ToListAsync());
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<SuperHero>>> DeleteSuperHero(int id)
+        {
+            var dbHero = await _context.SuperHeroes.FindAsync(id);
+            if(dbHero == null)
+            {
+                return BadRequest("Hero not found0");
+            }
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.SuperHeroes.ToListAsync());
+        }
+
+    }
+}
---
+++ SuperHeroNG/SuperHeroNG/Data/DataContext.cs
@@ -0,0 +1,12 @@
+namespace SuperHeroNG.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<SuperHero> SuperHeroes => Set<SuperHero>();
+    }
+}
